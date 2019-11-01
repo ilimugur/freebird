7d8fe5c3b538867a187acb8b99be50d3ec352885
Emirhan Bayyurt <ozanemirhan@gmail.com>
2019-11-02 02:58:30 +0300

    fugly plane physics for trying.

---
+++ Assets/AAA/Scripts/Game/CameraDirector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDirector : MonoBehaviour
+{
+
+    public PlaneController Target;
+    public float FollowSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        Vector3 newPos = transform.position;
+        newPos.x = Target.CameraTarget.x;
+        transform.position = newPos;
+        */
+        Vector3 newPos = Vector3.MoveTowards(transform.position, Target.CameraTarget, FollowSpeed);
+        newPos = new Vector3(Target.CameraTarget.x, newPos.y, -10f);
+        transform.position = newPos;
+    }
+}
--- Assets/AAA/Scripts/Game/PlaneController.cs
+++ Assets/AAA/Scripts/Game/PlaneController.cs
@@ -4,15 +4,84 @@ using UnityEngine;
 
 public class PlaneController : MonoBehaviour
 {
+    public Rigidbody2D RB;
+
+    public float VerticalSpeed;
+    public float MoveSpeed;
+
+    public GameObject CratePrefab;
+
+    public Vector3 CameraTarget;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        RB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateMethodSecond();
+    }
+
+    void UpdateMethodSecond()
+    {
+        RB.AddForce(Vector2.right * (MoveSpeed - transform.rotation.z), ForceMode2D.Force);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RB.AddForce(Vector2.up * VerticalSpeed, ForceMode2D.Impulse);
+
+            GameObject go = GameObject.Instantiate(CratePrefab, this.transform.position + new Vector3(0, -0.7f, 0), Quaternion.identity);
+        }
+
+        if (RB.velocity.y > 10f)
+        {
+            RB.velocity = new Vector2(RB.velocity.x, 10f);
+        }
+        else if (RB.velocity.y < -10f)
+        {
+            RB.velocity = new Vector2(RB.velocity.x, -10f);
+        }
+
+        if (RB.velocity.y > 0)
+        {
+            CameraTarget = transform.position + new Vector3(0f, 1 + transform.rotation.z);
+            transform.Rotate(Vector3.forward, 10f);
+            if (transform.rotation.z > 0.3f)
+                transform.rotation = new Quaternion(0, 0, 0.3f, 1f);
+        }
+        else if (RB.velocity.y < 0)
+        {
+            CameraTarget = transform.position + new Vector3(0f, -1 - transform.rotation.z);
+            transform.Rotate(Vector3.forward, -1f);
+            if (transform.rotation.z < -0.3f)
+                transform.rotation = new Quaternion(0, 0, -0.3f, 1f);
+        }
+    }
+
+    void UpdateMethodFirst()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RB.AddForce(Vector2.up * VerticalSpeed, ForceMode2D.Force);
+        }
+
+        if (RB.velocity.y > 0)
+        {
+            Quaternion rot = transform.rotation;
+            transform.Rotate(Vector3.forward, 1f);
+            if (transform.rotation.z > 0.3f)
+                transform.rotation = rot;
+        }
+        else if (RB.velocity.y < 0)
+        {
+            Quaternion rot = transform.rotation;
+            transform.Rotate(Vector3.forward, -1f);
+
+            if (transform.rotation.z < -0.3f)
+                transform.rotation = rot;
+        }
     }
 }
