1486f995fecae312423d7c030eed10ced5e89fe4
Emirhan Bayyurt <ozanemirhan@gmail.com>
2019-11-02 13:56:00 +0300

    Crate script updated. Cloud Atlas added.

---
+++ Assets/AAA/Scripts/Game/CloudAtlas.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudAtlas : MonoBehaviour
+{
+    public GameObject CloudPrefab;
+
+    public List<GameObject> Clouds;
+
+    public int CloudCount;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateInitialClouds();
+    }
+
+    private void GenerateInitialClouds()
+    {
+        for (int i = 0; i < (CloudCount / 3); i++)
+        {
+            GameObject go = GameObject.Instantiate(CloudPrefab, transform.position + new Vector3((i * 52f), 0f + UnityEngine.Random.Range(-5f, 5f), 0f), Quaternion.identity);
+            go.transform.SetParent(this.transform);
+            GameObject go2 = GameObject.Instantiate(CloudPrefab, transform.position + new Vector3((i * 52f) + UnityEngine.Random.Range(-5f, 5f), 17f + UnityEngine.Random.Range(-5f, 5f), 0f), Quaternion.identity);
+            go2.transform.SetParent(this.transform);
+            GameObject go3 = GameObject.Instantiate(CloudPrefab, transform.position + new Vector3((i * 52f) + UnityEngine.Random.Range(-5f, 5f), -17f + UnityEngine.Random.Range(-5f, 5f), 0f), Quaternion.identity);
+            go3.transform.SetParent(this.transform);
+            Clouds.Add(go);
+            Clouds.Add(go2);
+            Clouds.Add(go3);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Camera.main.transform);
+    }
+}
--- Assets/AAA/Scripts/Game/Crate.cs
+++ Assets/AAA/Scripts/Game/Crate.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class Crate : MonoBehaviour
 {
+    public Vector3 StartPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y < -6f)
+        if (transform.position.y < (StartPosition.y - 10f))
         {
             Destroy(this.gameObject);
         }
