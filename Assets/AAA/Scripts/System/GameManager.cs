57649745523d9f6ebcd1f4953efaab2c2b3f9706
serhat <serhatb@gmail.com>
2019-11-02 04:27:16 +0300

    instantiate landing strip and plane controller in game manager

--- Assets/AAA/Scripts/System/GameManager.cs
+++ Assets/AAA/Scripts/System/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using DG.Tweening;
 using MoreMountains.NiceVibrations;
 using UnityEngine;
 using UnityEngine.Analytics;
@@ -9,16 +10,26 @@ using Random = UnityEngine.Random;
 
 public class GameManager : Singleton<GameManager>
 {
-	public LandingStrip LandingStrip;
+	public LandingStrip LandingStripPrefab;
+	private LandingStrip LandingStrip;
+	public PlaneController PlaneControllerPrefab;
+	private PlaneController PlaneController;
+
 	void Awake()
 	{
 		Application.targetFrameRate = 60;
 
 		UIManager.Instance.ShowStartScreen(0);
-		EventManager.Instance.StartListening(Constants.EVENT_GAME_START, StartGame);
+		EventManager.Instance.StartListening(Constants.EVENT_LEVEL_START, StartGame);
+		EventManager.Instance.StartListening(Constants.EVENT_LEVEL_RESTART, RestartGame);
 
 		Load();
 
+		LandingStrip = Instantiate(LandingStripPrefab, this.transform);
+		PlaneController = Instantiate(PlaneControllerPrefab, LandingStrip.PlaneSpawnPosition.position, 
+			Quaternion.identity, this.transform);
+
+		PlaneController.SpawnLocation = LandingStrip.PlaneSpawnPosition.position;
 	}
 
 	void Start()
@@ -36,11 +47,19 @@ public class GameManager : Singleton<GameManager>
 		StartCoroutine(StartGameCo());
 	}
 
+	private void RestartGame()
+	{
+
+		StartCoroutine(StartGameCo());
+	}
+
 	private IEnumerator StartGameCo()
 	{
 		yield return new WaitForEndOfFrame();
 		Debug.Log("Starting Game");
 		Analytics.CustomEvent("StartGame");
+
+		DOTween.To(() => PlaneController.MoveSpeed, x => PlaneController.MoveSpeed = x, 5f, 2f);
 
 		//Game.StartCurrentLevel();
 		//EventManager.Instance.StopListening(GameConstants.EVENT_START_GAME, StartGame);
